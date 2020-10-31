d8727269f8aeceb8c61c1a084e201ee7a66922ce
TressOhi <fortressenebe@gmail.com>
2020-10-31 05:01:28 +0100

    modify condition to show slider nav arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,15 +50,15 @@ function setControl(){
     var cardWidth = window.getComputedStyle(card[0]).getPropertyValue('width')
     var container = document.getElementsByClassName('the-why-cards-container')[0]
     var diff = container.scrollWidth - container.clientWidth
-    cardWidth = parseInt(cardWidth) + 32
-
+    cardWidth = parseInt(cardWidth)
+    
     if(container.scrollLeft < cardWidth){
         document.getElementsByClassName('left-arrow-container')[0].style.display="none"
     }
     else{
         document.getElementsByClassName('left-arrow-container')[0].style.display="block"
     }
-    if(container.scrollLeft + cardWidth >= diff){
+    if(container.scrollLeft+10 >= diff){
         document.getElementsByClassName('right-arrow-container')[0].style.display="none"
     }
     else{
